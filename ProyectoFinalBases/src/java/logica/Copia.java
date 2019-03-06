bf00cd009955fedf7daacc9a7027b793adb12826
daniii3012 <daniel_301999@hotmail.com>
2019-03-05 22:35:46 -0500

    Create Copia.java

---
+++ ProyectoFinalBases/src/java/logica/Copia.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Copia {
+    private int idCopia; // k_copia
+    private String estadoPrestamoCopia; // i_estado_prestamo
+    private String estadoFisicoCopia; // i_estado_fisico
+    private String disponibleCopia; // i_disponible
+}
