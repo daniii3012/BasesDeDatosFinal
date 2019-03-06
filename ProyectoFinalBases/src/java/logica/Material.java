28e601cd9e96b332dd76cd3d96163d6a8becb187
daniii3012 <daniel_301999@hotmail.com>
2019-03-05 22:41:19 -0500

    Update Material.java

--- ProyectoFinalBases/src/java/logica/Material.java
+++ ProyectoFinalBases/src/java/logica/Material.java
@@ -10,5 +10,12 @@ package logica;
  * @author Daniel
  */
 public abstract class Material {
-    
+    private int idMaterial; // k_ISBNISSN
+    private String tituloMaterial; // n_titulo
+    private String tipoMaterial; // n_tipo
+    private String fechaPublicacionMaterial; // f_publicacion
+    private String temaMaterial; // n_tema
+    /*
+    Falta poner n_autor
+    */
 }
