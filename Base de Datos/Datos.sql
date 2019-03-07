e9a6382fec4b890d340c4f6ab82bf824d49e6172
daniii3012 <daniel_301999@hotmail.com>
2019-03-06 19:20:09 -0500

    Update Datos.sql

--- Base de Datos/Datos.sql
+++ Base de Datos/Datos.sql
@@ -1,5 +1,25 @@
-INSERT INTO bibliografico VALUES (1234567890123, 'Ejemplo', '06-03-2019', 'Libro', 'Este es un ejemplo');
+/*INSERT INTO bibliografico VALUES (1234567890123, 'Ejemplo', '06-03-2019', 'Libro', 'Este es un ejemplo');
 INSERT INTO bibliografico VALUES (1234567890124, 'Ejemplo1', '05-03-2019', 'Libro', 'Este es un ejemplo');
 INSERT INTO bibliografico VALUES (1234567890125, 'Ejemplo2', '12-06-1999', 'Revista', 'Este es un ejemplo de revista');
 
-INSERT INTO audiovisual VALUES (9876543210123, 'Ejemplo', '06-03-2019', 'Video', 'Este es un ejemplo de video');
+INSERT INTO audiovisual VALUES (9876543210123, 'Ejemplo', '06-03-2019', 'Video', 'Este es un ejemplo de video');*/
+
+INSERT INTO bibliografico VALUES (9788478884452, 'Harry Potter 1', '26-06-1997', 'Libro', 'Harry Potter y la piedra filosofal');
+INSERT INTO bibliografico VALUES (9788478884957, 'Harry Potter 2', '02-07-1998', 'Libro', 'Harry Potter y la cámara secreta');
+INSERT INTO bibliografico VALUES (9788478885190, 'Harry Potter 3', '08-07-1999', 'Libro', 'Harry Potter y el prisionero de Azkaban');
+INSERT INTO bibliografico VALUES (9788478886456, 'Harry Potter 4', '08-07-2000', 'Libro', 'Harry Potter y el cáliz de fuego');
+INSERT INTO bibliografico VALUES (9788478887422, 'Harry Potter 5', '21-06-2003', 'Libro', 'Harry Potter y la Orden del Fénix');
+INSERT INTO bibliografico VALUES (9788478889907, 'Harry Potter 6', '16-07-2005', 'Libro', 'Harry Potter y el misterio del príncipe');
+INSERT INTO bibliografico VALUES (9788498381405, 'Harry Potter 7', '21-07-2007', 'Libro', 'Harry Potter y las Reliquias de la Muerte');
+
+INSERT INTO bibliografico VALUES (9788498675399, 'Los Juegos del Hambre', '14-09-2008', 'Libro', 'Los Juegos del Hambre');
+INSERT INTO bibliografico VALUES (9788427200005, 'Los Juegos del Hambre 2', '01-09-2009', 'Libro', 'Los Juegos del Hambre 2 En Llamas');
+INSERT INTO bibliografico VALUES (9788427202146, 'Los Juegos del Hambre 3', '24-08-2010', 'Libro', 'Los Juegos del Hambre 3 Sinsajo');
+
+INSERT INTO bibliografico VALUES (9788427201187, 'Divergente', '25-04-2011', 'Libro', 'Divergente (novela)');
+INSERT INTO bibliografico VALUES (9788491870562, 'Insurgente', '01-05-2012', 'Libro', 'Insurgente (novela)');
+INSERT INTO bibliografico VALUES (9788491870586, 'Leal', '22-10-2013', 'Libro', 'Leal (novela)');
+
+INSERT INTO bibliografico VALUES (00279358, 'National Geographic', '01-10-1888', 'Revista', 'National Geographic Magazine');
+INSERT INTO bibliografico VALUES (00280836, 'Nature', '04-09-1869', 'Revista', 'Nature Magazine');
+INSERT INTO bibliografico VALUES (00368075, 'Science', '01-03-1880', 'Revista', 'Science Magazine');
